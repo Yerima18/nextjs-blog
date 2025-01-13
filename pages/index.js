8645c8d7126e47f9a6e456b2bceac0568511c384
lafia <adambagrilafia@gmail.com>
2025-01-13 23:33:00 +0300

    add the files

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData})  {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hey my name is lafia adam bagri. I am second year student in software engineering at the university of atlas which is located in istanbul,turkey</p>
+        <p>Hey my name is lafia adam bagri. I am a student in the university of gelisim which is located in istanbul,turkey</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
